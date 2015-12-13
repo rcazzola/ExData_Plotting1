386e07149f516d2ecc7e10ccfabdafc7175d621b
Rachel Cazzola <rcazzola@comcast.net>
2015-12-12 18:58:29 -0600

    plot

--- plot3.R
+++ plot3.R
@@ -5,15 +5,15 @@ data <- read.table("./Data/household_power_consumption.txt", header=T, sep=";", 
 subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #making the plot and saving the file
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+datetime <- strptime(paste(subset$Date, subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subset$Global_active_power)
+submetering1 <- as.numeric(subset$Sub_metering_1)
+submetering2 <- as.numeric(subset$Sub_metering_2)
+submetering3 <- as.numeric(subset$Sub_metering_3)
 
 png("plot3.png", width=480, height=480)
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+plot(datetime, submetering1, type="l", ylab="Energy sub metering",xlab=" ")
+lines(datetime, submetering2, type="l", col="red")
+lines(datetime, submetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1, col=c("black", "red", "blue"))
 dev.off()
--- plot4.R
+++ plot4.R
@@ -5,27 +5,23 @@ data <- read.table("./Data/household_power_consumption.txt", header=T, sep=";", 
 subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #making the plot and saving the file
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
-voltage <- as.numeric(subSetData$Voltage)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
-
+datetime <- strptime(paste(subset$Date, subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subset$Global_active_power)
+globalReactivePower <- as.numeric(subset$Global_reactive_power)
+voltage <- as.numeric(subset$Voltage)
+submetering1 <- as.numeric(subset$Sub_metering_1)
+submetering2 <- as.numeric(subset$Sub_metering_2)
+submetering3 <- as.numeric(subset$Sub_metering_3)
 
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
-
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
-
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
-
+plot(datetime, submetering1, type="l", ylab="Energy sub metering", xlab="")
+  lines(datetime, submetering2, type="l", col="red")
+  lines(datetime, submetering3, type="l", col="blue")
+  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=1, col=c("black", "red", "blue"), bty="o")
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
